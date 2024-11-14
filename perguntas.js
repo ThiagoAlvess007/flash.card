ffd1f6476a7f2e9cc8a406dffba76b54d37c4a90
ThiagoAlvess007 <00001091318335sp@al.educacao.sp.gov.br>
2024-11-14 13:48:22 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Esportes',
+    'Qual é o maior clube do Brasil?',
+    'O maior clube do Brasil é o São Paulo'
+)
+
+criaCartao(
+    'Esportes',
+    'Quem é o melhor goleiro da história?',
+    'O melhor goleiro da história é Rogério Ceni'
+)
+
+criaCartao(
+    'Esportes',
+    'O que o Palmeiras não tem?',
+    'O Palmeiras não tem mundial'
+)
+
+criaCartao(
+    'Esportes',
+    'Há quanto tempo o Corinthians não ganha um título importante?',
+    'O Corinthians não ganha um título importante há 7 anos'
+)
